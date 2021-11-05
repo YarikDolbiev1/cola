a90eb462abe492f78222cdf84b84fe9ab755d3fa
YarikDolbiev1 <90912350+YarikDolbiev1@users.noreply.github.com>
2021-11-05 19:37:47 +0200

    Add files via upload

---
+++ cola.js
@@ -0,0 +1,19 @@
+let products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+{ name: "Orange", calories: 160, color: "orange", sold: 12101 },
+{ name: "Cola", calories: 210, color: "caramel", sold: 25412 },
+{ name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+{ name: "Lemon", calories: 200, color: "clear", sold: 14983 },
+{ name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
+{ name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
+{ name: "Water", calories: 0, color: "clear", sold: 62123 }
+];
+function printProducts(products) {
+    for (let i = 0; i < products.length; i++) {
+      document.write("Name: " + products[i].name +
+        ", Calories: " + products[i].calories +
+        ", Color: " + products[i].color +
+        ", Sold: " + products[i].sold + "</br>");
+    }
+  }
+  
+printProducts(products);
